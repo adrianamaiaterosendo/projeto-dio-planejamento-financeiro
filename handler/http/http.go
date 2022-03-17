f7101c4815582b2b7da15deab58612d1d693d2d4
Adriana Maiate <66576219+adrianamaiaterosendo@users.noreply.github.com>
2022-03-17 12:36:14 -0300

    Adicionando Prometheus

--- handler/http/http.go
+++ handler/http/http.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/adrianamaiaterosendo/projeto-dio-planejamento-financeiro.git/handler/http/actuator"
 	"github.com/adrianamaiaterosendo/projeto-dio-planejamento-financeiro.git/handler/transaction"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 //Init é onde eu tenho as chamadas http
@@ -13,6 +14,8 @@ func Init() {
 	http.HandleFunc("/transactions", transaction.GetTransactions)
 	http.HandleFunc("/transsaction/create", transaction.CreatedTransactions)
 	http.HandleFunc("/health", actuator.Health)
+	//cria metricas da aplicação onde podemos customizar
+	http.Handle("/metrics", promhttp.Handler())
 
 	http.ListenAndServe(":8080", nil)
 }
